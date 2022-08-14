d0929b6ae00f606c18d0e457c0288fa6d316ff24
MinhHieuVux <minhhieuvux@gmail.com>
2022-08-14 16:30:24 +0700

    resf #3 login facebook google

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,28 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
 
+  # begin login gg facebook
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable , omniauth_providers: [:facebook, :google_oauth2]
+
+  def self.from_omniauth(auth)
+    result = User.where(email: auth.info.email).first
+    if result
+      return result
+    else
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        user.uid = auth.uid
+        user.provider = auth.provider
+        #  If you are using confirmable and the provider(s) you use validate emails
+        user.skip_confirmation!
+      end
+    end
+  end
+  # end login gg facebook
+
   # Returns a random token.
   def User.new_token
     SecureRandom.urlsafe_base64
